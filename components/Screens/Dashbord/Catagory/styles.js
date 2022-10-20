f0807fda991e801f354451f33827140cf8e9e03d
sha21img <shashankalw.img@gmail.com>
2022-10-20 12:21:59 +0530

    home

---
+++ components/Screens/Dashbord/Catagory/styles.js
@@ -0,0 +1,20 @@
+import {StyleSheet} from 'react-native';
+export const Styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'green',
+    padding: 15,
+  },
+  catagory: {
+    width: 85,
+    height: 85,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 100,
+    padding: 10,
+    marginRight: 10,
+  },
+  catagoryText: {
+    textAlign: 'center',
+    color: 'white',
+  },
+});
