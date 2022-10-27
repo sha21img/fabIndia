ddc40b4fb76cf9916507acb3c78424b846744b2e
GuptaVicky9462 <guptavicky.img@gmail.com>
2022-10-27 14:04:23 +0530

    Accordian change

--- components/Screens/Menu/index.js
+++ components/Screens/Menu/index.js
@@ -13,6 +13,7 @@ import {Colors} from '../../../assets/Colors';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import {Styles} from './styles';
+import Accordian from './Accordian';
 const data = [
   {
     id: 0,
@@ -181,78 +182,6 @@ const data1 = [
   'Terms of use',
   'Log out',
 ];
-const Accordian = ({title, category}) => {
-  const toggleAnimation = {
-    duration: 300,
-    update: {
-      duration: 300,
-      property: LayoutAnimation.Properties.opacity,
-      type: LayoutAnimation.Types.easeInEaseOut,
-    },
-    delete: {
-      duration: 200,
-      property: LayoutAnimation.Properties.opacity,
-      type: LayoutAnimation.Types.easeInEaseOut,
-    },
-  };
-  const [showContent, setShowContent] = useState(false);
-  const animationController = React.useRef(new Animated.Value(0)).current;
-  const toggleListItem = () => {
-    const config = {
-      duration: 300,
-      toValue: showContent ? 0 : 1,
-      useNativeDriver: true,
-    };
-    Animated.timing(animationController, config).start();
-    LayoutAnimation.configureNext(toggleAnimation);
-    setShowContent(!showContent);
-  };
-  const arrowTransform = animationController.interpolate({
-    inputRange: [0, 1],
-    outputRange: ['0deg', '180deg'],
-  });
-  return (
-    <>
-      <View style={Styles.accordbox}>
-        <TouchableOpacity onPress={() => toggleListItem()}>
-          <View
-            style={[
-              Styles.titlebox,
-              {
-                borderTopWidth: showContent ? 0 : 1,
-                backgroundColor: showContent ? '#F6F6F6' : '#ffffff',
-              },
-            ]}>
-            <Text
-              style={[
-                Styles.titletxt,
-                {
-                  fontWeight: showContent ? '700' : '400',
-                },
-              ]}>
-              {title}
-            </Text>
-            <Animated.View style={{transform: [{rotateZ: arrowTransform}]}}>
-              <MaterialIcons
-                name="keyboard-arrow-down"
-                color={Colors.primarycolor}
-                size={30}
-              />
-            </Animated.View>
-          </View>
-        </TouchableOpacity>
-        {showContent &&
-          category.map(item => {
-            return (
-              <View style={Styles.activebox}>
-                <Text style={Styles.activetxt}>{item}</Text>
-              </View>
-            );
-          })}
-      </View>
-    </>
-  );
-};
 
 export default function Menu() {
   return (
