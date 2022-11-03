47c437d3976266f862248205f1f77d119cdd9570
Pushkar-dev <pushkar.img@gmail.com>
2022-11-03 15:53:57 +0530

    furniture

--- components/Screens/Home/Catagory/FurnitureCategory/style.js
+++ components/Screens/Home/Catagory/FurnitureCategory/style.js
@@ -4,5 +4,11 @@ import Fonts from '../../../../../assets/fonts';
 const dimension = Dimensions.get('window');
 
 export const Styles = StyleSheet.create({
-  
+  chipbox: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    paddingHorizontal: 15,
+    marginTop: 10,
+  },
+  commontab: {marginLeft: 15, height: 500, marginBottom: 40},
 });
