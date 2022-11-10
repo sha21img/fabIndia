34d6112d830c56c7f46628042aee929b83347e9f
sha21img <shashankalw.img@gmail.com>
2022-11-10 12:42:06 +0530

    TopSwipper

--- components/Screens/HomeStack/index.js
+++ components/Screens/HomeStack/index.js
@@ -50,7 +50,7 @@ export default function HomeStack() {
   );
 
   return (
-    <Stack.Navigator initialRouteName="Home">
+    <Stack.Navigator initialRouteName="KidsCatagory">
       <Stack.Screen
         name="Home"
         component={Home}
