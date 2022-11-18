8d43c76d76d9fcb8b6c35868324a6459c19a313b
GuptaVicky9462 <guptavicky.img@gmail.com>
2022-11-18 17:35:34 +0530

    header design change

--- components/Screens/Home/HomeHeader/index.js
+++ components/Screens/Home/HomeHeader/index.js
@@ -6,28 +6,57 @@ import Ionicons from 'react-native-vector-icons/Ionicons';
 import {Styles} from './styles';
 import {image} from '../../../../assets/images';
 import Fonts from '../../../../assets/fonts';
+import SimpleLineIcons from 'react-native-vector-icons/SimpleLineIcons';
+import EvilIcons from 'react-native-vector-icons/EvilIcons';
+
+import {Colors} from '../../../../assets/Colors';
 
 export default function HomeHeader() {
   return (
     <View style={Styles.container}>
-      <View style={Styles.logoBox}>
-        <Image source={image.color_logo} style={Styles.logo} />
-      </View>
+      <SimpleLineIcons name="menu" color={Colors.primarycolor} size={20} />
+
       <View style={Styles.detailContainer}>
         <TouchableOpacity style={Styles.locationContainer}>
-          <Ionicons name="location-sharp" color={'#792C27'} size={20} />
+          <EvilIcons name="search" color={Colors.primarycolor} size={30} />
+
+          {/* <Ionicons name="location-sharp" color={'#792C27'} size={20} />
           <Text numberOfLines={1} style={Styles.locationText}>
             Powai, Mumbai
-          </Text>
+          </Text> */}
         </TouchableOpacity>
         <TouchableOpacity style={Styles.currencyContainer}>
-          <Text style={Styles.currencyIcon}>₹</Text>
-          <Text style={Styles.currencyText}>INR</Text>
+          <EvilIcons name="heart" color={Colors.primarycolor} size={30} />
+
+          {/* <Text style={Styles.currencyIcon}>₹</Text>
+          <Text style={Styles.currencyText}>INR</Text> */}
         </TouchableOpacity>
         <TouchableOpacity style={Styles.cartContainer}>
-          <AntDesign name="shoppingcart" size={24} color={'#792C27'} />
+          <EvilIcons name="cart" size={30} color={Colors.primarycolor} />
         </TouchableOpacity>
       </View>
     </View>
   );
+}
+{
+  /* <View style={Styles.container}>
+<View style={Styles.logoBox}>
+  <Image source={image.color_logo} style={Styles.logo} />
+</View>
+<View style={Styles.detailContainer}>
+  <TouchableOpacity style={Styles.locationContainer}>
+    <Ionicons name="location-sharp" color={'#792C27'} size={20} />
+    <Text numberOfLines={1} style={Styles.locationText}>
+      Powai, Mumbai
+    </Text>
+  </TouchableOpacity>
+  <TouchableOpacity style={Styles.currencyContainer}>
+    <Text style={Styles.currencyIcon}>₹</Text>
+    <Text style={Styles.currencyText}>INR</Text>
+  </TouchableOpacity>
+  <TouchableOpacity style={Styles.cartContainer}>
+    <AntDesign name="shoppingcart" size={24} color={'#792C27'} />
+  </TouchableOpacity>
+</View>
+</View> */
 }
