6368065cd289f319f8e59ee9adc8ff5bb310a87f
GuptaVicky9462 <guptavicky.img@gmail.com>
2022-11-21 15:47:04 +0530

    common-tab API

--- components/Screens/HomeStack/index.js
+++ components/Screens/HomeStack/index.js
@@ -55,7 +55,7 @@ export default function HomeStack() {
   );
 
   return (
-    <Stack.Navigator initialRouteName="LandingPageSaris">
+    <Stack.Navigator initialRouteName="Home">
       <Stack.Screen
         name="Home"
         component={Home}
